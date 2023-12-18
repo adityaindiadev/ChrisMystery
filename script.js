4def18c1016cb06a5e1dff8b44fe762350de3a26
“adityaindiadev” <aditya.india.dev@gmail.com>
2023-12-18 16:33:05 +0530

    Almost Complete

--- script.js
+++ script.js
@@ -1,44 +1,50 @@
+const buttonReload = document.getElementById("buttonReload")
+buttonReload.addEventListener("click", reload);
 var quiz = {
     data: [
-        {
-            // q: "What is your Car No ?",
-            q: "xxxxxx",
-            o: [
-                "50 meters",
-                "70 meters",
-                "100 meters",
-                "120 meters"],
-
-            a: 1
-        },
-        {
-            // q: "How many members are in Team ProH2R ?",
-            q: "yyyyy",
-            o: [
-                "22",
-                "24",
-                "32",
-                "36"],
-
-            a: 2
-        },
 
         {
-            // q: "What is the meaning of nile ?",
+            // q: "What is the meaning of `Nile` ?",
             q: "zzzz",
             o: [
-                "400 pounds",
-                "550 pounds",
-                "700 pounds",
-                "750 pounds"],
+                "River",
+                "Place",
+                "Ocean",
+                "Sea"],
+
+            a: 0
+        },
+
+        {
+            // q: "What is your Car No (Number Plate) ?",
+            q: "xxxxxx",
+            o: [
+                "DL 5AF 5845",
+                "DL 5AF5845",
+                "DL 2DF 5845",
+                "DL 5CF 5845"],
 
             a: 3
         },
+        {
+            // q: "How many employees are in Team ProH2R ?",
+            q: "yyyyy",
+            o: [
+                "15",
+                "12",
+                "17",
+                "16"],
+
+            a: 3
+        },
+
+        
 
     ],
 
     hWrap: null, // HTML quiz container
     hQn: null, // HTML question wrapper
+    hint: null, // HTML question wrapper
     hAns: null, // HTML answers wrapper
 
     now: 0, // current question
@@ -51,6 +57,12 @@ var quiz = {
         quiz.hQn.id = "quizQn";
         quiz.hWrap.appendChild(quiz.hQn);
 
+        //SS hint
+        quiz.hint = document.createElement("div");
+        quiz.hint.id = "lastHint";
+        quiz.hWrap.appendChild(quiz.hint);
+        quiz.hint.className = "buttonReloadHide";
+
         quiz.hAns = document.createElement("div");
         quiz.hAns.id = "quizAns";
         quiz.hWrap.appendChild(quiz.hAns);
@@ -59,6 +71,7 @@ var quiz = {
     },
 
     draw: () => {
+        buttonReload.classList.add("buttonReloadHide")
         quiz.hQn.innerHTML = quiz.data[quiz.now].q;
 
         quiz.hAns.innerHTML = "";
@@ -95,7 +108,10 @@ var quiz = {
         setTimeout(() => {
             if (quiz.now < quiz.data.length) { quiz.draw(); }
             else {
+                // buttonReload.classList.remove("buttonReloadHide")
+                quiz.hint.classList.remove("buttonReloadHide")
                 quiz.hQn.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`; quiz.hAns.innerHTML = "";
+                quiz.hint.innerHTML = `🔔 In Your Office Find "PLEASE SHUT THE DOOR AFTER ENTRY/EXIT"`;
             }
         }, 500);
     }, reset: () => {
@@ -105,4 +121,10 @@ var quiz = {
     }
 };
 
+function reload() {
+    console.log("dd");
+    location.reload();
+}
+
 window.addEventListener("load", quiz.init);
+
